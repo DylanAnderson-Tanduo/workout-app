efc8fbbedde8d29ff5cb9846529eced2fcd279e5
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-24 16:32:13 -0600

    Setup Project Structure and test component

---
+++ workout-proj/src/App.js
@@ -0,0 +1,11 @@
+import Exercises from "./components/Exersises";
+
+function App() {
+  return (
+    <div className="App">
+     <Exercises/>
+    </div>
+  );
+}
+
+export default App;
---
+++ workout-proj/src/components/Exersises.js
@@ -0,0 +1,9 @@
+const Exercises = (props) => {
+  return (
+    <div>
+        <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default Exercises;
