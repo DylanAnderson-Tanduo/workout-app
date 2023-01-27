fd523dd889950308a13da0453cbb1fe34b1326a2
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-27 13:41:20 -0600

    Made exercises display all information

--- workout-proj/src/components/Exersises.js
+++ workout-proj/src/components/Exersises.js
@@ -8,7 +8,13 @@ const Exercises = ({ exersiseList, workout }) => {
   return (
     <div>
       {filteredArray.map((exercise, index) => {
-        return <h1 key={index}>{exercise.name}</h1>;
+        return (
+          <div key={index}>
+            <h1>{exercise.name}</h1>
+            <p>Reps: {exercise.reps}</p>
+            <p>Weight: {exercise.weight}</p>
+          </div>
+        );
       })}
     </div>
   );
